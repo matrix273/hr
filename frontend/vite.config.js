1799c12e685b206afee3d8545f6238e723613ca6
matrix273 <matrix273@gmail.com>
2026-06-12 16:51:46 +0800

    chore: 补充 vite 构建配置 (base, outDir, assetsDir)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -13,6 +14,10 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
   assetsInclude: ['**/*.md']
 })
